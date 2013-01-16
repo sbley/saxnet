b0c83e669b20566748dd0c47dfedf49f5b45955c
Stefan Bley <stefan.bley@NB170.saxsys.de>
2013-01-16 22:33:40 +0100

    remove comments

--- src/main/java/de/saxsys/saxnet/SaxnetApplication.java
+++ src/main/java/de/saxsys/saxnet/SaxnetApplication.java
@@ -8,33 +8,15 @@ import javafx.stage.Stage;
 
 public class SaxnetApplication extends Application {
 
-    public static Stage stage;
+	public static Stage stage;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-    public void start(Stage _stage) throws Exception
-    {
-        stage = _stage;
-
-		// primaryStage.setTitle("Hello World!");
-		// Button btn = new Button();
-		// btn.setText("Say 'Hello World'");
-		// btn.setOnAction(new EventHandler<ActionEvent>() {
-		//
-		// @Override
-		// public void handle(ActionEvent event) {
-		// System.out.println("Hello World!");
-		// }
-		// });
-		//
-		// StackPane root = new StackPane();
-		// root.getChildren().add(btn);
-		// primaryStage.setScene(new Scene(root, 300, 250));
-		// primaryStage.show();
-
+	public void start(Stage _stage) throws Exception {
+		stage = _stage;
 		Parent root = FXMLLoader.load(getClass().getResource("saxnet.fxml"));
 
 		stage.setTitle("Saxnet");
