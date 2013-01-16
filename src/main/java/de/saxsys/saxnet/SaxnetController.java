8cba8aa515b3e1aef378623a3e7624d54b7fa772
Stefan Bley <stefan.bley@saxsys.de>
2013-01-16 19:40:47 +0100

    bla

--- src/main/java/de/saxsys/saxnet/SaxnetController.java
+++ src/main/java/de/saxsys/saxnet/SaxnetController.java
@@ -8,12 +8,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+
 public class SaxnetController implements Initializable {
+
 	@FXML
 	private ListView<String> listEmployees;
 
 	@FXML
 	private TextField txtName;
+
+	private EmployeeRepository employeeRepo;
 
 	@Override
 	public void initialize(URL url, ResourceBundle bundle) {
@@ -21,7 +27,12 @@ public class SaxnetController implements Initializable {
 	}
 
 	public void insertEmployees() {
-		listEmployees.getItems().addAll("Sebastian", "Alex");
+		// "Sebastian", "Alex"
+		Transaction tx = NeoDB.getInstance().beginTx();
+		Node node1 = NeoDB.getInstance().getNodeById(1);
+		tx.finish();
+		listEmployees.getItems().addAll(
+				new String[] { (String) node1.getProperty("name") });
 	}
 
 	@FXML
